222c2bbf9e4a392f199c31b9852549255d0f060a
zhangtao <zhangtao@chelun.com>
2018-04-10 10:19:57 +0800

    change the format

--- PrepareCppDev.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-#!/usr/bin/env python
-##########################################################
-#   Teach Wisedom To Machine.
-#   Please Call Me Programming devil.
-#
-#   Prepare to build environment for cpp devolpers
-#########################################################
--- Template.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-##########################################################
-#   A clever person solves a problem. A wise person avoids it
-#   Please call Me programming devil.
-#
-#
-######################################################## #
