6abe49f6a1476e000be7d3e6edf49b0e3eb24f9c
tao.zhang <tao.zhang@hiscene.com>
2019-03-30 13:52:20 +0800

    Optimize echo Services

--- nettest/nc.py
+++ nettest/nc.py
@@ -1,48 +1,74 @@
 # -*- coding: utf-8 -*-
+"""
+In conjunction with nc.py for network testing,
+this service provides a echo instruction
+and remote system instruction operation
+
+tazzhang  2019-4-1
+"""
+
+
 import socket
 import sys
 import time
-socket.setdefaulttimeout(5)
+
+socket.setdefaulttimeout(60)
 
 
-def get_tick_count():
-    current_time=time.time()
+def gettickcount():
+    current_time = time.time()
     return int(round(current_time * 1000))
 
 
-def prob1(ip, port):
-    addr = (ip, int(port) )
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("socket() fileno:{0}".format(s.fileno()))
-    print("connect() addr:{0} ... ...".format(addr))
-    t1 = get_tick_count()
-    try:
-        s.connect(addr)
-        use_tick = get_tick_count() - t1
-        print("connect() addr:{0};ok;use_tick:{1}".format(addr, use_tick))
-    except socket.error as arg:
-        print(arg)
-        # use_tick = core_utils.get_tick_count() - t1
-        # errno, err_msg = arg
-        # print("connect server(%s) failed: %s, errno=%d use_tick=%d" % (addr, err_msg, errno, use_tick) )
-        sys.exit(1)
-
+def process_msg(s):
     while True:
-        inp = raw_input("please input:\n>>>")
-        print(":::", inp)
-        if inp == "q":
-            print("END")
+        inp = raw_input(">>>")
+        if inp == "end":
             s.sendall(inp)
-            sys.exit(1)
-        else:
-            t1 = get_tick_count()
-            bytes_sent = s.sendall(inp)
-            print("sendall:", bytes_sent, "data:", inp)
-            result = s.recv(1024 * 8)
-            use_tick = get_tick_count() - t1
-            print("use_tick=%d userecv:%s" % (use_tick, result))
+            return
 
+        t1 = gettickcount()
+        s.sendall(inp)
+        t2 = gettickcount()
+        t = t2 - t1
+        print("send>{}>use:{}".format(inp, t))
+
+        t1 = gettickcount()
+        try:
+            data = s.recv(1024 * 16)
+            t2 = gettickcount()
+            t = t2 - t1
+            print("---------------------")
+            print(data)
+        except Exception as e:
+            print(e)
+            return
+
+
+def client(address):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_info = "{0}_{1}".format(s.fileno(), address)
+    t1 = gettickcount()
+    try:
+        s.connect(address)
+        t2 = gettickcount()
+        t = t2 - t1
+        print("connect ok.{0} ".format(client_info, t))
+    except socket.error as arg:
+        t2 = gettickcount()
+        t = t2 - t1
+        print("connect failed.{0} ".format(client_info, t))
+        print(arg)
+        sys.exit(1)
+    process_msg(s)
+
+
+def main():
+    if len(sys.argv) < 3:
+        print("help:python nc.py [ip] [port]")
+        return
+    address = (sys.argv[1], int(sys.argv[2]) )
+    client(address)
 
 if __name__ == '__main__':
-    print (sys.argv)
-    prob1(sys.argv[1], sys.argv[2])
+    main()
